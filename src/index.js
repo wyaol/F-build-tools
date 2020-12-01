6ce1b9e7db3034a75782359433aefe01ba0ce6c9
wyao <289672494@qq.com>
2020-12-01 15:06:05 +0800

    finish

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './styles/reboot.scss';
+
+const App = () => (
+  <div>
+    hello world
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
